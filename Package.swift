87b46a5b9a944919366712d8bdf7d4960e8230e4
Akshay Kumar <akshay.kumar@system73.com>
2026-06-24 15:11:08 +0530

    update the starscream and swiftprotobuff on nexus and fix the compiler error

--- Package.swift
+++ Package.swift
@@ -29,13 +29,13 @@ let package = Package(
             ),
             .binaryTarget(
                 name: "Starscream",
-                url:"https://artifacts.s73cloud.com/repository/maven-s73-releases/s73-polynet-plat/polynet-ios-starscream-xcframework/4.0.8.03.01.2026/polynet-ios-starscream-xcframework-4.0.8.03.01.2026.zip",
-                checksum: "ee75df480afee8b7aeda21189f71f8bfb8c13e8e254bff8f73be5c44c5e64b6d"
+                url:"https://artifacts.s73cloud.com/repository/maven-s73-releases/s73-polynet-plat/polynet-ios-starscream-xcframework/4.0.8.24.06.2026/polynet-ios-starscream-xcframework-4.0.8.24.06.2026.zip",
+                checksum: "71a19787d1a32c6098a3563fb9871c38187143d1a43eb833b73817360c0430d9"
             ),
             .binaryTarget(
                 name: "SwiftProtobuf",
-                url:"https://artifacts.s73cloud.com/repository/maven-s73-releases/s73-polynet-plat/polynet-ios-swift-protobuf-xcframework/1.26.0.03.01.2026/polynet-ios-swift-protobuf-xcframework-1.26.0.03.01.2026.zip",
-                checksum: "cd783820f3ed183ca756fc45e6ae14a3eaf2b46c2e48f8b0212662a2b7dbb301"
+                url:"https://artifacts.s73cloud.com/repository/maven-s73-releases/s73-polynet-plat/polynet-ios-swift-protobuf-xcframework/1.26.0.24.06.2026/polynet-ios-swift-protobuf-xcframework-1.26.0.24.06.2026.zip",
+                checksum: "5bd333b5914291114d56171372fde48fddc2db0e1c5ea891ba713f68b9763e01"
             )
         ]
 )
